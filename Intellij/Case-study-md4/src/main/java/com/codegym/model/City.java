f77e8af9b173194fa9ebbb25eaff158495e091ec
Tsunami <doanhongson050999@gmail.com>
2021-08-13 11:41:36 +0700

    done

---
+++ Intellij/Case-study-md4/src/main/java/com/codegym/model/City.java
@@ -0,0 +1,77 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+
+@Entity
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToOne
+    private Country country;
+    private double area;
+    private long population;
+    private double GPD;
+    private String description;
+
+    public City() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+
+    public double getGPD() {
+        return GPD;
+    }
+
+    public void setGPD(double GPD) {
+        this.GPD = GPD;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ Intellij/src/main/java/com/codegym/BaithiMd4Application.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaithiMd4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaithiMd4Application.class, args);
+    }
+
+}
